16720395da732a5770024c8e24fde9f649be2824
joancolina819 <37549304+joancolina819@users.noreply.github.com>
2019-03-27 01:12:26 -0500

    mas reglas

--- ReglasIA/src/mundo/AnalisisSuelo.java
+++ ReglasIA/src/mundo/AnalisisSuelo.java
@@ -26,4 +26,73 @@ public class AnalisisSuelo {
 	        System.out.println("==> ph: NEUTRO");
 		}
 	}
+	
+	public void rule3() {
+		if (suelo.getPh()>=5.2&&suelo.getPh()<=6.2) {
+			System.out.println("\n\n\n");
+
+	        System.out.println("==> ph: LIGERAMENTE ACIDO");
+
+		}
+	}
+	
+	
+	public void rule4() {
+		if (suelo.getPh()<=5.6) {
+			System.out.println("\n\n\n");
+
+	        System.out.println("==> ph: ACIDO");
+
+		}
+	}
+	public void rule5() {
+		if (suelo.getConductividadElectrica()<=0.8) {
+			System.out.println("\n\n\n");
+
+	        System.out.println("==> conductividadElectrica: BAJA");
+
+
+		}
+	}
+	
+	public void rule6() {
+		if (suelo.getConductividadElectrica()>=0.8) {
+			System.out.println("\n\n\n");
+
+	        System.out.println("==> conductividadElectrica: ALTA");
+
+		}
+	}
+	
+	public void rule7() {
+		if (suelo.getPH()==Suelo.ALCALINO) {
+			System.out.println("\n\n\n");
+
+	        System.out.println("==> ExtractoSoluble: True");
+
+		}
+	}
+	
+	public void rule8() {
+		if (suelo.getPH()==Suelo.LIGERAMENTE_ALCALINO) {
+			System.out.println("\n\n\n");
+
+	        System.out.println("==> ExtractoSoluble: True");
+
+
+		}
+	}
+	
+	public void rule9() {
+		if (suelo.getArcilla()>=40.0&&suelo.getPH()==Suelo.ALCALINO&&suelo.getConductividadElectrica()==Suelo.ALTA) {
+			System.out.println("\n\n\n");
+
+	        System.out.println("==> ExtractoSoluble: True");
+
+
+		}
+	}
+	
+	
+	
 }
--- ReglasIA/src/mundo/Suelo.java
+++ ReglasIA/src/mundo/Suelo.java
@@ -2,13 +2,25 @@ package mundo;
 
 public class Suelo {
 
-	
-	
+	public static final String ALCALINO = "alcalino";
+	public static final String LIGERAMENTE_ALCALINO = "alcalino";
+	public static final double ALTA = 1000;
 	private double ph;
 	private double conductividadElectrica;
 	private double arcilla;
 	private double arena;
 	private double limo;
+	private String PH;
+	
+	
+	public String getPH() {
+		return PH;
+	}
+
+	public void setPH(String pH) {
+		PH = pH;
+	}
+
 	public double getPh() {
 		return ph;
 	}
@@ -16,5 +28,38 @@ public class Suelo {
 	public void setPh(double ph) {
 		this.ph = ph;
 	}
+
+	public double getConductividadElectrica() {
+		return conductividadElectrica;
+	}
+
+	public void setConductividadElectrica(double conductividadElectrica) {
+		this.conductividadElectrica = conductividadElectrica;
+	}
+
+	public double getArcilla() {
+		return arcilla;
+	}
+
+	public void setArcilla(double arcilla) {
+		this.arcilla = arcilla;
+	}
+
+	public double getArena() {
+		return arena;
+	}
+
+	public void setArena(double arena) {
+		this.arena = arena;
+	}
+
+	public double getLimo() {
+		return limo;
+	}
+
+	public void setLimo(double limo) {
+		this.limo = limo;
+	}
+	
 	
 }

31b363b54614d7f3894a24504782bf4f4fec9f82
joancolina819 <37549304+joancolina819@users.noreply.github.com>
2019-03-27 00:50:21 -0500

    reglas

---
+++ ReglasIA/src/mundo/AnalisisSuelo.java
@@ -0,0 +1,16 @@
+package mundo;
+
+public class AnalisisSuelo {
+
+	private Suelo suelo;
+
+	public void rule1() {
+		if (suelo.getPh()>7.2) {
+
+			System.out.println("\n\n\n");
+
+	        System.out.println("==> ph: ALCALINO");
+
+		}
+	}
+}
---
+++ ReglasIA/src/mundo/Suelo.java
@@ -0,0 +1,20 @@
+package mundo;
+
+public class Suelo {
+
+	
+	
+	private double ph;
+	private double conductividadElectrica;
+	private double arcilla;
+	private double arena;
+	private double limo;
+	public double getPh() {
+		return ph;
+	}
+
+	public void setPh(double ph) {
+		this.ph = ph;
+	}
+	
+}

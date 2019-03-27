0cf1e8735e1dc40bf8500e7cd909a8a607e73a2a
joancolina819 <37549304+joancolina819@users.noreply.github.com>
2019-03-27 00:57:19 -0500

    mas reglas

--- ReglasIA/src/mundo/AnalisisSuelo.java
+++ ReglasIA/src/mundo/AnalisisSuelo.java
@@ -4,13 +4,26 @@ public class AnalisisSuelo {
 
 	private Suelo suelo;
 
-	public void rule1() {
+	public void rule0() {
 		if (suelo.getPh()>7.2) {
-
+			System.out.println("\n\n\n");
+	        System.out.println("==> ph: ALCALINO");
+		}
+	}
+	
+	public void rule1() {
+		if (suelo.getPh()>6.8&&suelo.getPh()<6.8) {
 			System.out.println("\n\n\n");
 
-	        System.out.println("==> ph: ALCALINO");
+	        System.out.println("==> ph: LIGERAMENTE ALCALINO");
+		}
+	}
+	
+	public void rule2() {
+		if (suelo.getPh()>=6.2&&suelo.getPh()<=6.8) {
+			System.out.println("\n\n\n");
 
+	        System.out.println("==> ph: NEUTRO");
 		}
 	}
 }
